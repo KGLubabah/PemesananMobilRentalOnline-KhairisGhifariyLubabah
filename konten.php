0c0dd6492777b102bcb2eb62349ac9db449263f9
KGLubabah <khairis_ghifariy_27rpl@student.smktelkom-mlg.sch.id>
2019-04-27 23:17:20 +0700

    commit kedua

--- konten.php
+++ konten.php
@@ -8,7 +8,7 @@
         </div>
 		<br>
 		<div class="topnav">
-            <h2 class="huruf">Pemesanan Rental Mobil Online</h2>
+            <h2>Pemesanan Rental Mobil Online</h2>
         </div>
 		<br>
 		<div class="row">
@@ -58,7 +58,7 @@
 				</tr>
 				<tr>
 					<td>6</td>
-					<td>Toyota Hiace</td>
+					<td>Toyota Fortuner</td>
 					<td>Rp1.100.000</td>
 					<td>6</td>
 					<td>2</td>
@@ -68,7 +68,48 @@
 		</div>
 		<br>
         <div class="botnav">
-			<a href="#">PESAN</a>
+			<a href="http://localhost/Tugas%20Akhir/Pesan.html">PESAN</a>
         </div>
+		<br>
+		<div class="botnav2">
+			<h2 class="huruf">KENDARAAN</h2>
+		</div>
+		<br>
+		<div class="row">
+			<div class="leftcolumn">
+				<div class="card">
+					<div class="avanza" style="heigt:300px; width:80%;" align="left;"></div>
+				</div>
+			</div>
+			<div class="rightcolumn">
+				<div class="card">
+					<div class="ertiga" style="heigt:300px; width:80%;"></div>
+				</div>
+			</div>
+			<div class="leftcolumn2">
+				<div class="card">
+					<div class="innova" style="heigt:300px; width:80%;"></div>
+				</div>
+			</div>
+			<div class="rightcolumn2">
+				<div class="card">
+					<div class="hiace" style="heigt:300px; width:80%;"></div>
+				</div>
+			</div>
+			<div class="leftcolumn3">
+				<div class="card">
+					<div class="elf" style="heigt:300px; width:80%;"></div>
+				</div>
+			</div>
+			<div class="rightcolumn3">
+				<div class="card">
+					<div class="fortuner" style="heigt:300px; width:80%;"></div>
+				</div>
+			</div>
+		</div>
+		<br>
+	<marquee bgcolor="Green" behavior="scroll" direction="right" scrollamount="8"> <font face="Times New Rowman" 
+	color="silver" > &copy; Khairis Ghifariy Lubabah - Pemesanan Rental Mobil Online</font>
+	</marquee>
 	</body>
 </html>
---
+++ tugas_akhir.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2019 at 04:40 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tugas_akhir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `tipe_mobil` int(3) NOT NULL,
+  `mobil` text NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jml_kursi` int(11) NOT NULL,
+  `jml_koper` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(3) NOT NULL,
+  `username` varchar(8) NOT NULL,
+  `password` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'Haris', 'rentalmo');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`tipe_mobil`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `tipe_mobil` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
